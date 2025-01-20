6a8bfbb221fffae55ee4399f488fd90225790b8f
Vaishnavi Saraf <100290198+VaishnaviSaraf@users.noreply.github.com>
2025-01-20 09:52:05 +0530

    Name and age updated

---
+++ student_data.java
@@ -0,0 +1,17 @@
+package git_60;
+import java.util.*;
+public class student_data {
+    public static void main(String srg[])
+    {
+    	Scanner sc=new Scanner(System.in);
+    	String name,surname;
+    	int age;
+    	System.out.println("Enter your first name : ");
+    	name=sc.nextLine();
+    	System.out.println("Enter your Last name : ");
+    	surname=sc.nextLine();
+    	System.out.println("Enter your age : ");
+    	age=sc.nextInt();
+    	System.out.println("My name is "+name+" "+surname+" "+"I am "+age+" Years old.");
+    }
+}
